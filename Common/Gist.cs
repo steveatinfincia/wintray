bb0f183ec8defafaf97a2b2db6dbe3d7733ff567
Stephen Oliver <steve@infincia.com>
2017-09-24 15:21:15 -0400

    Use background thread for Gist

--- Common/Gist.cs
+++ Common/Gist.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.IO;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 
 using Newtonsoft.Json.Serialization;
@@ -13,58 +14,60 @@ namespace Common.Github {
         public string Location { get; set; }
 
         public static void Create(string log, string title, Action<Gist> success, Action<string> failure) {
-            try {
+            Task.Factory.StartNew(() => {
+                try {
 
-                var fileName = $"FreenetTray - {title}.txt";
+                    var fileName = $"FreenetTray - {title}.txt";
 
-                var file = new JObject(
-                    new JProperty("content", log)
-                );
+                    var file = new JObject(
+                        new JProperty("content", log)
+                    );
 
-                JArray files = new JArray {
-                new JProperty(fileName, file)
-            };
+                    JArray files = new JArray {
+                        new JProperty(fileName, file)
+                    };
 
-                JObject payLoad = new JObject(
-                    new JProperty("files", files),
-                    new JProperty("description", title),
-                    new JProperty("public", true)
-                );
+                    JObject payLoad = new JObject(
+                        new JProperty("files", files),
+                        new JProperty("description", title),
+                        new JProperty("public", true)
+                    );
 
-                var headers = new WebHeaderCollection() {
-                    ["Content-Type"] = "application/vnd.github.v3+json",
-                    ["Accept"] = "application/json",
-                    ["User-Agent"] = "FreenetTray"
-                };
+                    var headers = new WebHeaderCollection() {
+                        ["Content-Type"] = "application/vnd.github.v3+json",
+                        ["Accept"] = "application/json",
+                        ["User-Agent"] = "FreenetTray"
+                    };
 
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create($"https://{Constants.FNGithubAPI}/gists");
-                httpWebRequest.ContentType = "application/json";
-                httpWebRequest.Method = "POST";
-                httpWebRequest.Headers = headers;
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create($"https://{Constants.FNGithubAPI}/gists");
+                    httpWebRequest.ContentType = "application/json";
+                    httpWebRequest.Method = "POST";
+                    httpWebRequest.Headers = headers;
 
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
 
-                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream())) {
+                    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream())) {
 
-                    string json = payLoad.ToString();
+                        string json = payLoad.ToString();
 
-                    streamWriter.Write(json);
-                    streamWriter.Flush();
-                    streamWriter.Close();
+                        streamWriter.Write(json);
+                        streamWriter.Flush();
+                        streamWriter.Close();
+                    }
+
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream())) {
+                        var result = streamReader.ReadToEnd();
+
+                        Gist response = JsonConvert.DeserializeObject<Gist>(result);
+
+                        success(response);
+                    }
+                } catch (Exception ex) {
+                    failure(ex.Message);
                 }
-
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream())) {
-                    var result = streamReader.ReadToEnd();
-
-                    Gist response = JsonConvert.DeserializeObject<Gist>(result);
-
-                    success(response);
-                }
-            } catch (Exception ex) {
-                failure(ex.Message);
-            }
+            });
         }
     }
 }
